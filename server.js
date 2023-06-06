0f5a26dd025efea1ccd5a9f52a1f93794265b368
Vitor Nicchio <vitornicchio@Vitors-MacBook-Pro.local>
2023-06-06 20:04:24 -0300

    Add nodemon

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const http = require('http');
-
 const port = 3000;
+
+const routes = {
+    '/': 'Node course',
+    '/books': 'Books page',
+    '/authors': 'Authors page',
+    '/about': 'About page'
+}
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Curso de Node');
+    res.end(routes[req.url]);
 })
 
 server.listen(port, () => {
