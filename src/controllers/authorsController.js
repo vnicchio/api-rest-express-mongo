3a64502015195564b299a526051fa62fde708345
Vitor Nicchio Alves <vitornicchioalves@gmail.com>
2023-06-10 17:05:13 -0300

    Refactor to use async/await

--- src/controllers/authorsController.js
+++ src/controllers/authorsController.js
@@ -1,23 +1,32 @@
 import authors from "../models/Author.js";
 
 class AuthorController {
-	static getAuthors = (req, res) => {
-		authors.find({}).then(result => {
+	static getAuthors = async (req, res) => {
+		try {
+			const result = await authors.find({});
+
 			res.status(200).json(result);
-		});
+		} catch (error) {
+			res.status(500).json({message: error.message});
+		}
 	};
 
-	static getAuthorById = (req, res) => {
-		const id = req.params.id;
+	static getAuthorById = async (req, res) => {
+		try {
+			const id = req.params.id;
+			const author = await authors.findById(id);
 
-		authors.findById(id).then(author => {
-			res.status(200).json(author);
-		}).catch(err => {
-			res.status(500).send({message: err.message});
-		});
+			if (author !== null) {
+				res.status(200).send(author);
+			} else {
+				res.status(404).send({message: "Author doesn't exist"});
+			}
+		} catch (error) {
+			res.status(500).send({message: error.message});
+		}
 	};
 
-	static postAuthor = (req, res) => {
+	static postAuthor = async (req, res) => {
 		let author = new authors(req.body);
 
 		author.save().then(result => {
@@ -27,24 +36,28 @@ class AuthorController {
 		});
 	};
 
-	static putAuthor = (req, res) => {
-		const id = req.params.id;
+	static putAuthor = async (req, res) => {
+		try {
+			const id = req.params.id;
 
-		authors.findByIdAndUpdate(id, req.body).then(author => {
+			const author = authors.findByIdAndUpdate(id, req.body);
+
 			res.status(200).json(author);
-		}).catch(err => {
-			res.status(500).send({message: err.message});
-		});
+		} catch (error) {
+			res.status(500).send({message: error.message});
+		}
 	};
 
-	static deleteAuthor = (req, res) => {
-		const id = req.params.id;
+	static deleteAuthor = async (req, res) => {
+		try {
+			const id = req.params.id;
 
-		authors.findByIdAndDelete(id).then(() => {
+			await authors.findByIdAndDelete(id);
+
 			res.status(200).send({message: "The author has been deleted!"});
-		}).catch(err => {
-			res.status(500).send({message: err.message});
-		});
+		} catch (error) {
+			res.status(500).send({message: error.message});
+		}
 	};
 }
 
--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -1,60 +1,75 @@
 import books from "../models/Book.js";
 
 class BookController {
-	static getBooks = (req, res) => {
-		books.find().populate("author").exec().then(result => {
+	static getBooks = async (req, res) => {
+		try {
+			const result = await books.find().populate("author").exec();
+
 			res.status(200).json(result);
-		});
+		} catch (error) {
+			res.status(500).send({message: error.message});
+		}
 	};
 
-	static getBookById = (req, res) => {
-		const id = req.params.id;
+	static getBookById = async (req, res) => {
+		try {
+			const id = req.params.id;
+			let book = await books.findById(id).populate("author", "name").exec();
 
-		books.findById(id).populate("author", "name").exec().then(book => {
-			res.status(200).json(book);
-		}).catch(err => {
-			res.status(500).send({message: err.message});
-		});
+			if (book !== null) {
+				res.status(200).send(book);
+			} else {
+				res.status(404).send({message: "Author doesn't exist"});
+			}
+		} catch (error) {
+			res.status(500).send({message: error.message});
+		}
 	};
 
-	static getBookByPublisher = (req, res) => {
-		const publisher = req.query.publisher;
+	static getBookByPublisher = async (req, res) => {
+		try {
+			const publisher = req.query.publisher;
 
-		books.find({"publisher": publisher}, {}).then(books => {
-			res.status(200).send(books);
-		}).catch(err => {
-			res.status(500).send({message: err.message});
-		});
+			let result = await books.find({"publisher": publisher}, {});
+			res.status(200).send(result);
+		} catch (error) {
+			res.status(500).send({message: error.message});
+		}
 	};
 
-	static postBook = (req, res) => {
-		let book = new books(req.body);
+	static postBook = async (req, res) => {
+		try {
+			let book = new books(req.body);
+			let result = await book.save();
 
-		book.save().then(result => {
 			res.status(201).send(result.toJSON());
-		}).catch(err => {
-			res.status(500).send({message: err.message});
-		});
+		} catch (error) {
+			res.status(500).send({message: error.message});
+		}
 	};
 
-	static putBook = (req, res) => {
-		const id = req.params.id;
+	static putBook = async (req, res) => {
+		try {
+			const id = req.params.id;
+			const result = await books.findByIdAndUpdate(id, req.body);
 
-		books.findByIdAndUpdate(id, req.body).then(book => {
-			res.status(200).json(book);
-		}).catch(err => {
-			res.status(500).send({message: err.message});
-		});
+			res.status(200).json(result);
+
+		} catch (error) {
+			res.status(500).send({message: error.message});
+		}
 	};
 
-	static deleteBook = (req, res) => {
-		const id = req.params.id;
+	static deleteBook = async (req, res) => {
+		try {
+			const id = req.params.id;
+			await books.findByIdAndDelete(id);
 
-		books.findByIdAndDelete(id).then(() => {
 			res.status(200).send({message: "The book has been deleted!"});
-		}).catch(err => {
-			res.status(500).send({message: err.message});
-		});
+
+		} catch (error) {
+			res.status(500).send({message: error.message});
+		}
 	};
 }
 
